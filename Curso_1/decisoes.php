357f6085e4c05b7d1a0331df24b7a0ff3ec29907
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2021-08-20 00:40:58 -0300

    Sobre decisões if e else

--- Curso_1/decisoes.php
+++ Curso_1/decisoes.php
@@ -1,11 +1,11 @@
 <?php
 
-$idade = 21;
+$idade = 13;
 
 echo "Você só pode entrar se tiver mais do que 18 anos." . PHP_EOL;
 
 if($idade >= 18){
-    echo("Você tem $idade anos. PODE entrar");
+    echo("Você tem $idade anos. PODE entrar!!!");
 }else{
-    echo "Você tem menos de 18 anos, NÃO pode entrar";
+    echo "Você tem menos de 18 anos, NÃO pode entrar!!!!!";
 }
