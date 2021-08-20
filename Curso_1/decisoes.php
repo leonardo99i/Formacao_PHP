cda9e61f117fc20c3cf9d3b2bd0218d8f79dd52e
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2021-08-20 00:55:48 -0300

    Arrumando indentação

--- Curso_1/decisoes.php
+++ Curso_1/decisoes.php
@@ -1,16 +1,14 @@
 <?php
 
-$idade = 21;
-$numeroDePessoas = 0;
+$idade = 16;
+$numeroDePessoas = 2;
 
 echo "Você só pode entrar se tiver mais do que 18 anos." . PHP_EOL;
 
 if($idade >= 18){
     echo("Você tem $idade anos. PODE entrar!!!");
+}else if($idade >= 16 and $numeroDePessoas = 1){
+    echo "Você tem $idade anos, está acompanhado(a), então pode entrar.";
 }else{
-    if($idade >= 16 and $numeroDePessoas = 1){
-        echo "Você tem $idade anos, está acompanhado(a), então pode entrar.";
-    }else{
-        echo "Você tem menos de $idade anos e não está acompanhado, NÃO pode entrar!!!!!";
-    }
+    echo "Você tem menos de $idade anos e não está acompanhado, NÃO pode entrar!!!!!";
 }
